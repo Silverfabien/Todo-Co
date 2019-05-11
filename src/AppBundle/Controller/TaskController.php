b3f428fc6a54c9efe84e09f80da327adc3b1c474
fabien hollebeque <hollebeque.fabien@hotmail.com>
2019-05-11 19:57:12 +0200

    Modification of names

--- src/AppBundle/Controller/TaskController.php
+++ src/AppBundle/Controller/TaskController.php
@@ -101,23 +101,13 @@ class TaskController extends Controller
     }
 
     /**
-     * @Route("/tasks/{id}", name="task_view")
-     */
-    public function viewTaskAction($id)
-    {
-        $task = $this->getDoctrine()->getManager()->getRepository(Task::class)->findOneBy(['id' => $id]);
-
-        return $this->redirectToRoute('task_view', ['id' => $task]);
-    }
-
-    /**
      * @Route("/task", name="task_load")
      */
     public function ajaxGetTask(Request $request)
     {
-        $test = ['template2' => $this->render('task/item.html.twig',
+        $task = ['tasks' => $this->render('task/item.html.twig',
             ['tasks' => $this->getDoctrine()->getRepository('AppBundle:Task')
                 ->findAllTask($request->get('page'))])->getContent()];
-        return new JsonResponse($test);
+        return new JsonResponse($task);
     }
 }
