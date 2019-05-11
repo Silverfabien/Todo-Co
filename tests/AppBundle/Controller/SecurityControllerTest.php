1ae414e5a25007d0336c874e1369e61150a5874b
fabien hollebeque <hollebeque.fabien@hotmail.com>
2019-05-11 20:11:14 +0200

    Addition of functional tests

---
+++ tests/AppBundle/Controller/SecurityControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use AppBundle\Controller\SecurityController;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+    private $client = null;
+
+    public function setUp()
+    {
+        $this->client = static::createClient([], [
+            'PHP_AUTH_USER' => 'Silversat',
+            'PHP_AUTH_PW' => 'Shafheux'
+        ]);
+    }
+
+    public function testLoginPage()
+    {
+        $this->client->request('GET', '/login');
+
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testLogoutPage()
+    {
+        $this->client->request('GET', '/logout');
+
+        $this->assertSame(302, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testLoginCheck()
+    {
+        $this->expectException(\Exception::class);
+        $controller = new SecurityController();
+        $controller->loginCheck();
+    }
+
+    public function testLogout()
+    {
+        $this->expectException(\Exception::class);
+        $controller = new SecurityController();
+        $controller->logout();
+    }
+
+}
---
+++ tests/AppBundle/Controller/UserControllerTest.php
@@ -0,0 +1,112 @@
+<?php
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserControllerTest extends WebTestCase
+{
+    private $client = null;
+
+    public function setUp()
+    {
+        $this->client = static::createClient([], [
+            'PHP_AUTH_USER' => 'Silversat',
+            'PHP_AUTH_PW' => 'Shafheux'
+        ]);
+    }
+
+    public function testListUserPage()
+    {
+        $this->client->request('GET', '/users');
+
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testListUserPageIfNotLogin()
+    {
+        $client = static::createClient([], []);
+        $client->request('GET', '/users');
+
+        $this->assertSame(302, $client->getResponse()->getStatusCode());
+    }
+
+    public function testCreateUserPage()
+    {
+        $this->client->request('GET', '/users/create');
+
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testCreateUserPageIfNotLogin()
+    {
+        $client = static::createClient([], []);
+        $client->request('GET', '/users/create');
+
+        $this->assertSame(302, $client->getResponse()->getStatusCode());
+    }
+
+    public function testEditUserIfExistPage()
+    {
+        $this->client->request('GET', '/users/1/edit');
+
+        $this->assertSame(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testEditUserPageIfExistPageAndIfNotLogin()
+    {
+        $client = static::createClient([], []);
+        $client->request('GET', '/users/1/edit');
+
+        $this->assertSame(302, $client->getResponse()->getStatusCode());
+    }
+
+    public function testEditUserIfNotExistPage()
+    {
+        $this->client->request('GET', '/users/a/edit');
+
+        $this->assertSame(404, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testEditUserPageIfNotExistPageAndIfNotLogin()
+    {
+        $client = static::createClient([], []);
+        $client->request('GET', '/users/a/edit');
+
+        $this->assertSame(302, $client->getResponse()->getStatusCode());
+    }
+
+    public function testCreateUserForm()
+    {
+        $crawler = $this->client->request('GET', '/users/create');
+
+        $form = $crawler->selectButton('Ajouter')->form();
+
+        $form['user[username]'] = 'Un nouveau nom';
+        $form['user[password][first]'] = '12345';
+        $form['user[password][second]'] = '12345';
+        $form['user[email]'] = 'unemail@test.fr';
+
+        $this->client->submit($form);
+        $crawler = $this->client->followRedirect();
+
+        $this->assertSame(1, $crawler->filter('div.alert.alert-success.flash-block')->count());
+    }
+
+    public function testEditUserForm()
+    {
+        $crawler = $this->client->request('GET', '/users/4/edit');
+
+        $form = $crawler->selectButton('Modifier')->form();
+
+        $form['edit_user[username]'] = 'Un nom modifié';
+        $form['edit_user[password][first]'] = '54321';
+        $form['edit_user[password][second]'] = '54321';
+        $form['edit_user[email]'] = 'unemailmodifier@test.fr';
+        $form['edit_user[roles]'] = ["ROLE_USER"];
+
+        $this->client->submit($form);
+        $crawler = $this->client->followRedirect();
+
+        $this->assertSame(1, $crawler->filter('div.alert.alert-success.flash-block')->count());
+    }
+}
