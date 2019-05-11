32b97ba5f7845d32aa218709c06a37e92b4005b2
fabien hollebeque <hollebeque.fabien@hotmail.com>
2019-05-11 19:58:22 +0200

    Added repository and task view management

--- src/AppBundle/Repository/TaskRepository.php
+++ src/AppBundle/Repository/TaskRepository.php
@@ -1,10 +1,20 @@
 <?php
 
-
 namespace AppBundle\Repository;
 
+use Doctrine\ORM\EntityRepository;
 
-class TaskRepository
+class TaskRepository extends EntityRepository
 {
+    public function findAllTask($page = 6)
+    {
+        $limit = $page * 1;
 
+        $query = $this->createQueryBuilder('t')
+            ->orderBy('t.createdAt', 'ASC')
+            ->setMaxResults($limit)
+            ->getQuery();
+
+        return $query->getResult();
+    }
 }
